6f81c0a04d5091a51a06e5d25df83ec4b14dae3e
Cesar Sarmiento <itsupport3@myopulence.com>
2025-03-20 15:38:42 -0400

    Installing Api just in case

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+// })->middleware('auth:sanctum');
