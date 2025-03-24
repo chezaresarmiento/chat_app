89bdd19fb7f7c1f6e0a5acc428761f164774114a
Cesar Sarmiento <itsupport3@myopulence.com>
2025-03-24 11:31:06 -0400

    added the route to extract messages after joining room

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MessageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Models\Message;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -23,5 +26,23 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/getMessages/{roomId}', function($roomId)
+    {
+        $user = auth()->user();
+        $lastLogin = session('last_login_at');
+
+        $messages = Message::where('created_at', '>', $lastLogin)
+            ->where('roomid', $roomId)
+            ->with('user:id,name')
+            ->orderBy('created_at', 'asc')
+            ->get();
+
+        return response()->json($messages);
+    });
+
+//use MessageController to post messages
+Route::post('/messages', [MessageController::class, 'store'])->middleware('auth')->name('messages.store');
+
 
 require __DIR__.'/auth.php';
