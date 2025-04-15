7d87068f54c9f699d534d0f706d2643251d74221
Cesar Sarmiento <itsupport3@myopulence.com>
2025-04-14 22:27:30 -0400

    added last_login property

--- routes/web.php
+++ routes/web.php
@@ -30,13 +30,24 @@ Route::middleware('auth')->group(function () {
 Route::get('/getMessages/{roomId}', function($roomId)
     {
         $user = auth()->user();
-        $lastLogin = session('last_login_at');
+        $lastLogin = session('last_login');
 
         $messages = Message::where('created_at', '>', $lastLogin)
             ->where('roomid', $roomId)
             ->with('user:id,name')
             ->orderBy('created_at', 'asc')
-            ->get();
+            ->get()->map(function ($message) {
+                // Merge user properties into top-level array
+                return [
+                    'id' => $message->id,
+                    'name' => $message->user->name ?? 'Unknown',
+                    'content' => $message->content,
+                    'roomid' => $message->roomid,
+                    'created_at' => $message->created_at->format('M d, Y h:i'),
+                    'updated_at' => $message->updated_at,
+                ];
+            });
+
 
         return response()->json($messages);
     });
